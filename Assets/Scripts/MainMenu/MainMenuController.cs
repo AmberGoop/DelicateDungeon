055ad6a4a4939eaf4fdea36f85d234d231a3e5a4
MintiestKitKat <AmberGoop@gmail.com>
2026-07-24 22:09:25 +0200

    Add Intermission Scene, Remove Hard Mode

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame() {
+        SceneManager.LoadScene("Intermission");
+    }
+    public void quitGame() {
+        Application.Quit();
+    }
+}
