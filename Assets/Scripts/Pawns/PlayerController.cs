4b8d9bc6b94b0c230465f13030a5c2c2f91f17e7
MintiestKitKat <AmberGoop@gmail.com>
2026-07-25 17:04:55 +0200

    Add Player Controller

---
+++ Assets/Scripts/Pawns/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public float moveSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        playerRB.AddForce(direction.normalized*moveSpeed*Time.deltaTime);
+        /*if(direction.magnitude< 0.01f) {
+            playerRB.linearVelocity = Vector3.zero;
+        }*/
+
+    }
+}
