f501065b07692eb309cb0df121f5b63fdc2becce
MintiestKitKat <AmberGoop@gmail.com>
2026-07-27 18:09:52 +0200

    Enemy Animation Framework

--- Assets/Scripts/Pawns/EnemyController.cs
+++ Assets/Scripts/Pawns/EnemyController.cs
@@ -10,7 +10,16 @@ public class EnemyController : MonoBehaviour
     };
     public State currentState = State.Idle;
     public int timer = 60;
-    public bool facingLeft = true;
+    private SpriteRenderer sprite;
+    private Animator animator;
+    public int animationTimer=0;
+    public string currentAnimation= "";
+    public Sprite spriteIdle1;
+    public Sprite spriteIdle2;
+    public Sprite spriteWalk1;
+    public Sprite spriteWalk2;
+    public Sprite spriteAtk1;
+    public Sprite spriteAtk2;
 
     public int baseMoveSpeed = 3000;
     public int fastMoveSpeed = 4500;
@@ -34,6 +43,8 @@ public class EnemyController : MonoBehaviour
     {
         health = maxHealth;
         enemyRB = GetComponent<Rigidbody>();
+        sprite = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+        animator = transform.Find("Sprite").GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         hitbox = transform.GetChild(0).gameObject.GetComponent<BoxCollider>();
         healthDisplay = transform.Find("HealthDisplay").Find("HealthNum").GetComponent<TMPro.TextMeshProUGUI>();
@@ -42,12 +53,19 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animationTimer++;
+
         healthDisplay.text = ""+health;
         if(invuln>0){invuln--;}
         toPlayer=player.transform.position-transform.position;
         toPlayer.y=0;
         switch(currentState){
             case State.Idle:
+                if((animationTimer/30)%2==0) {
+                    sprite.sprite=spriteIdle1;
+                } else {
+                    sprite.sprite=spriteIdle2;
+                }
                 timer-=1;
                 if(timer<1){
                     currentState=State.Roam;
@@ -56,6 +74,11 @@ public class EnemyController : MonoBehaviour
                 }
                 break;
             case State.Roam:
+                if((animationTimer/30)%2==0) {
+                    sprite.sprite=spriteWalk1;
+                } else {
+                    sprite.sprite=spriteWalk2;
+                }
                 timer-=1;
                 enemyRB.AddForce(moveDirection.normalized*baseMoveSpeed*Time.deltaTime);
                 if(timer<1){
@@ -64,6 +87,11 @@ public class EnemyController : MonoBehaviour
                 }
                 break;
             case State.Aggro:
+                if((animationTimer/30)%2==0) {
+                    sprite.sprite=spriteWalk1;
+                } else {
+                    sprite.sprite=spriteWalk2;
+                }
                 moveDirection=toPlayer;
                 if(timer>0){timer-=1;}
                 if(toPlayer.magnitude<atkRange) {
@@ -77,8 +105,12 @@ public class EnemyController : MonoBehaviour
                 break;
             case State.Attack:
                 timer-=1;
+                if(timer==119) {
+                    sprite.sprite=spriteAtk1;
+                }
                 if(timer==105){
                     hitbox.enabled=true;
+                    sprite.sprite=spriteAtk2;
                 }
                 if(timer==100){
                     hitbox.enabled=false;
@@ -87,6 +119,7 @@ public class EnemyController : MonoBehaviour
                     if(toPlayer.magnitude>atkRange){
                         currentState=State.Aggro;
                     }
+                    sprite.sprite=spriteIdle1;
                 }
                 if(timer<1){
                     timer=120;
@@ -104,9 +137,20 @@ public class EnemyController : MonoBehaviour
         if(currentState==State.Aggro&&toPlayer.magnitude>=loseRange) {
             currentState=State.Idle;
         }
+        if(moveDirection.x>0){
+            changeAnimation("turnCCW");
+        } else {
+            changeAnimation("turnCW");
+        }
 
 
+    }
 
+    private void changeAnimation(string animation) {
+        if(currentAnimation!=animation) {
+            currentAnimation=animation;
+            animator.Play(animation);
+        }
     }
 
 }
